0e0cd60241765bccd3088bc2bc7eeea72d160bd7
qingfengsuiyu <xiaoyihao2016@gmail.com>
2018-01-31 21:46:06 +0800

    add update files

---
+++ team_2/Lesson7/Update/ukf.h
@@ -0,0 +1,49 @@
+//
+// Created by 郝晓翼 on 2018/1/31.
+//
+
+#ifndef UPDATE_UKF_H
+#define UPDATE_UKF_H
+
+#endif //UPDATE_UKF_H
+
+#include "Eigen/Dense"
+#include <vector>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+class UKF{
+public:
+    UKF();
+
+    virtual ~UKF();
+
+    void Init();
+
+    void GenerateSigmaPoints(MatrixXd* Xsig_out);
+    void AugmentedSigmaPoints(MatrixXd* Xsig_out);
+    void SigmaPointPrediction(MatrixXd* Xsig_out);
+    void PredictMeanAndCovariance(VectorXd* x_pred, MatrixXd* P_pred);
+    void PredictRadarMeasurement(VectorXd* z_out, MatrixXd* S_out);
+    void UpdateState(VectorXd* x_out, MatrixXd* P_out);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
